b948d586992606b9f0d71dc1e05bcbe56f7633bd
DMyers82 <dmyers19@woh.rr.com>
2024-10-04 22:29:12 -0400

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMyCalculatorApp
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}

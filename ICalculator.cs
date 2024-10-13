d3eff2c89cd41d0f2ca888333e595db91233a27b
DMyers82 <dmyers19@woh.rr.com>
2024-10-13 18:20:24 -0400

    Added a Moq Test

---
+++ ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMyCalculatorApp
+{
+    public interface ICalculator
+    {
+        int Add(int value1, int value2);
+        int Subtract(int value1, int value2);
+        int Multiply(int value1, int value2);
+        int Divide(int value1, int value2);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var calc = new Calculator();
+            Console.WriteLine("Add: " + calc.Add(1, 1));
+            Console.WriteLine("Subtract: " + calc.Subtract(100, 10));
+            Console.WriteLine("Multiply: " + calc.Multiply(5, 5));
+            Console.WriteLine("Divide: " + calc.Divide(400, 20));
+            Console.ReadLine();
         }
     }
 }
